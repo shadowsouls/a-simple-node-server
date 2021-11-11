7254efcf10b0c4ad9367c443071a8d0037ea8dc4
覆影 <2415277546@qq.com>
2021-11-11 09:45:55 +0800

    通过url获取路径名

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const http = require('http');
+const url = require('url');
 
-function onRequest(request, response) {
-    console.log("Request received.");
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.write("Hello World");
-    response.end();
-}
+
 
 function start() {
+
+    function onRequest(request, response) {
+
+         let pathname = url.parse(request.url).pathname// parse是旧版api
+        // const pathname = new URL(request.url).pathname
+        console.log(request.url);
+        console.log(pathname);
+        console.log(`Request for  received.`);
+        response.writeHead(200, { "Content-Type": "text/plain" });
+        response.write("Hello World");
+        response.end();
+    }
+
     http.createServer(onRequest).listen(8888)
     console.log("Server has started.");
 }
 
-exports.start=start
+exports.start = start
