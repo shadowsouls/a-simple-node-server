cddb2ae59248a4c6836a5758de7362f67d7ee138
覆影 <2415277546@qq.com>
2021-11-17 17:32:40 +0800

    开始文件传输的教学

--- server.js
+++ server.js
@@ -6,15 +6,22 @@ const url = require('url');
 function start(route, handle) {
 
     function onRequest(request, response) {
+        let postData = ''
+        var pathname = url.parse(request.url).pathname;
+        console.log("Request for " + pathname + " received.");
 
-        let pathname = url.parse(request.url).pathname// parse是旧版api
-        // const pathname = new URL(request.url).pathname
-        console.log(`-------------------------`);
-        console.log(`Request for ${pathname} received.`);
+        request.setEncoding("utf8");
 
+        // 在这里监听传入的数据流
+        request.addListener('data',(postDataChunk) => {
+            postData+=postDataChunk
+            console.log(`Received Post data chunk ${postDataChunk}`);
+        })
+
+        request.addListener('end',() => {
+            route(handle,pathname,response,postData)
+        })
         
-        let content = route(handle, pathname, response)
-
 
     }
 
